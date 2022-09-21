fb2da75844fceb874a77a93c9ff6dd9a172b8a4a
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-09-21 13:51:35 -0300

    Update style.css

--- main.js
+++
@@ -1,7 +0,0 @@
-let menu = document.getElementById("#menu");
-let sidebar = document.querySelector(".sidebar");
-let hideLogo = document.getElementById("logo");
-
-function close() {
-  sidebar.classList.toggle("close");
-};

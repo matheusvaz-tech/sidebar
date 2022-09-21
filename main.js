995d584874725a72f6d75183ebc41c503eb6f0d3
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-09-21 11:28:52 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("#menu");
+let sidebar = document.querySelector(".sidebar");
+let hideLogo = document.getElementById("logo");
+
+function close() {
+  sidebar.classList.toggle("close");
+};
